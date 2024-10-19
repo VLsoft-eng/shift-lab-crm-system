7199e2b4d83250dfc1d2b8084ceba5556bfdc95f
VL <deeeaddrop@gmail.com>
2024-10-19 17:12:10 +0700

    feat(domain): add seller not found exception Add SellerNotFoundException for situations when seller with current id not exist in db

---
+++ src/main/java/ru/shift/task/domain/exception/SellerNotFoundException.java
@@ -0,0 +1,7 @@
+package ru.shift.task.domain.exception;
+
+public class SellerNotFoundException extends RuntimeException {
+    public SellerNotFoundException(Long id) {
+        super("Seller with id " + id + " not found");
+    }
+}
