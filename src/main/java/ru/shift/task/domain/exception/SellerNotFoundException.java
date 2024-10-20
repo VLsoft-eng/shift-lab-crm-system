7446e9b1c65efb24e016d0e14b2ede9fe6732f17
VL <deeeaddrop@gmail.com>
2024-10-20 12:35:21 +0700

    update(domain): add second constructor for seller not found exception

--- src/main/java/ru/shift/task/domain/exception/SellerNotFoundException.java
+++ src/main/java/ru/shift/task/domain/exception/SellerNotFoundException.java
@@ -4,4 +4,8 @@ public class SellerNotFoundException extends RuntimeException {
     public SellerNotFoundException(Long id) {
         super("Seller with id " + id + " not found");
     }
+
+    public SellerNotFoundException(String message) {
+        super(message);
+    }
 }
