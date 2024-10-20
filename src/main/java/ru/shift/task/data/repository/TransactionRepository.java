a9fc16600383dd66d5752eb8a3a39a518c15b666
VL <deeeaddrop@gmail.com>
2024-10-20 14:04:48 +0700

    feat(domain): add custom query in TransactionRepository Add method for find seller by id

--- src/main/java/ru/shift/task/data/repository/TransactionRepository.java
+++ src/main/java/ru/shift/task/data/repository/TransactionRepository.java
@@ -1,7 +1,13 @@
 package ru.shift.task.data.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import ru.shift.task.data.entity.Transaction;
 
+import java.util.List;
+
 public interface TransactionRepository extends JpaRepository<Transaction, Long> {
+    @Query("SELECT t FROM Transaction t WHERE t.seller.id = :sellerId")
+    List<Transaction> findBySellerId(@Param("sellerId") Long sellerId);
 }
