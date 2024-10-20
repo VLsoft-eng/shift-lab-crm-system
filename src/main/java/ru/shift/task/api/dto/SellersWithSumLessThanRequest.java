139cd4cfc5b7c67b30fcb95f7ce88188325d993f
VL <deeeaddrop@gmail.com>
2024-10-20 18:11:14 +0700

    update(api): add annotation builder to SellersWithSumLessThanRequest

--- src/main/java/ru/shift/task/api/dto/SellersWithSumLessThanRequest.java
+++ src/main/java/ru/shift/task/api/dto/SellersWithSumLessThanRequest.java
@@ -1,9 +1,11 @@
 package ru.shift.task.api.dto;
 
 import jakarta.validation.constraints.NotNull;
+import lombok.Builder;
 
 import java.time.LocalDateTime;
 
+@Builder
 public record SellersWithSumLessThanRequest(
         @NotNull(message = "Start time point is required") LocalDateTime start,
         @NotNull(message = "End time point is required") LocalDateTime end,
